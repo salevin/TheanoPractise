5af7e51e21f1db35fc58658930fcf0547ffd9d47
salevin <samueljlevin@gmail.com>
2015-10-04 21:09:03 -0400

    New TODOs

--- Practise.py
+++ Practise.py
@@ -14,4 +14,7 @@ import matplotlib.cm as cm
 import matplotlib.pyplot as plt
 
 if __name__ == '__main__':
-    print
+    x = [1.47, 1.50, 1.52, 1.55, 1.57, 1.60, 1.63, 1.65, 1.68, 1.70, 1.73, 1.75, 1.78, 1.80, 1.83]
+    y = [52.21, 53.12, 54.48, 55.84, 57.20, 58.57, 59.93, 61.29, 63.11, 64.47, 66.28, 68.10, 69.92, 72.19, 74.46]
+
+    # TODO http://underflow.fr/ai/lets-play-with-theano-547  https://en.wikipedia.org/wiki/Simple_linear_regression
